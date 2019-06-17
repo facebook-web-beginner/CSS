33f63a26ba44fdd1ea1e3de5fb316605aeb49ecd
Retro <mossiat.jeoffrey@outlook.com>
2019-06-17 14:30:53 +0200

    Add triangle shape

---
+++ Animate-Canvas/Shapes/Triangle.js
@@ -0,0 +1,50 @@
+
+
+
+// On attends que le DOM complet sois charger.
+window.addEventListener("DOMContentLoaded", (event) => {
+    
+console.log("DOM entièrement chargé et analysé");
+ 
+
+var triangle = document.getElementById("triangle");
+
+function triangleDraw ()
+    {
+     
+        
+        if (triangle.getContext)
+        {
+    
+            var ctx = triangle.getContext('2d');
+        }
+
+        // Si le canvas n'est pas pris en charge on fais un rapport.
+        else
+        {
+            alert(" Le canvas ID : triangle,  n'est pas pris en charge par votre navigateur.")
+        }
+        
+
+        
+        
+                ctx.beginPath();
+
+                ctx.moveTo(25, 100);
+    
+                ctx.lineTo(100, 100);
+    
+                ctx.lineTo(62, 25);
+                ctx.lineTo(25, 100);
+    
+                ctx.stroke();
+            
+        
+
+        
+    }
+    
+    triangleDraw();
+
+
+ });
